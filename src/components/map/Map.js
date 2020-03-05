5a49a2cfce5a4d60b47590c5229da78d38adce53
nevets615 <surj316@yahoo.com>
2020-03-04 23:45:44 -0500

    working on map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,1 +1,20 @@
-import { Graph } from "react-d3-graph";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import GamePad from "../gamePad/GamePad";
+
+const GamePad = () => {
+  const [gameState, setGameState] = useState({});
+  useEffect(() => {
+    axios
+      .get("https://lambda-mud-test.herokuapp.com/api/adv/init", {
+        Authorization: localStorage.getItem("token")
+      })
+      .then(res => {});
+  }, []);
+  return (
+    <div>
+      <GamePad />
+    </div>
+  );
+};
+export default Game;
