b75e7cee69e97b23c2065857816d9b3ab00f195f
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2020-03-04 23:45:36 -0500

    pulled in changes from feature branch

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,12 +1,23 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 // Components
 import GamePad from "../gamePad/GamePad";
 
 const Game = () => {
+const [gameState, setGameState] = useState({});
+
+useEffect(()=> {
+axios.get("https://lambda-mud-test.herokuapp.com/api/adv/init", {
+  Authorization: localStorage.getItem("token")
+})
+.then((res)=> {
+  console.log("data" , res.data)
+})
+} ,[] )
+
   return (
     <div>
-      
+
       <GamePad />
     </div>
   );
