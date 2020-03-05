b5c23dcde66d0c8cdef4f0ad2282a341895eabf6
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-03-04 18:14:14 -0800

    Style login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
 
 // Components
 import GamePad from "./components/gamePad/GamePad";
+import Login from "./components/login/Login";
 
 function App() {
   return (
     <div>
+      <Route path="/login" component={Login} exact />
       <GamePad />
     </div>
   );
