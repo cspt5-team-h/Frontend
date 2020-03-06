d32ef9722a599bb5c4aafb6f21cdbb9c7149dcbb
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2020-03-05 23:49:49 -0500

    gamepad wired up to change rooms

--- src/components/gamePad/GamePad.js
+++ src/components/gamePad/GamePad.js
@@ -1,25 +1,51 @@
 import React from "react";
 import "./gamePad.css";
+import axios from "axios";
 
 import triangle from "../../assets/icons/Triangle.svg";
 
 const GamePad = () => {
+
+  const onClickHandler =(e) => {
+
+  // e.preventDefault()
+  axios
+  .post("https://lambda-mud-test.herokuapp.com/api/adv/move", {direction: e}, {
+    headers: {
+      authorization: `Token ${localStorage.getItem("token")}`
+    }
+  })
+  
+  }
+
+
+
+
+
   return (
     <div className="gamepad-container">
-      <button className="control">
+      <button className="control"
+      onClick= {e=> onClickHandler("n")}
+      >
         <img src={triangle} alt="move north control" />
       </button>
 
       <div className="gamepad-we-cardinal-container">
-        <button className="control control-left">
+        <button className="control control-left"
+         onClick= {e=> onClickHandler("w")}
+        >
           <img src={triangle} alt="move west control" />
         </button>
-        <button className="control control-right">
+        <button className="control control-right"
+         onClick= {e=> onClickHandler("e")}
+        >
           <img src={triangle} alt="move east control" />
         </button>
       </div>
 
-      <button className="control control-down">
+      <button className="control control-down"
+       onClick= {e=> onClickHandler("s")}
+      >
         <img src={triangle} alt="move south control" />
       </button>
     </div>
