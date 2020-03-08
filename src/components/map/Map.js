74f99f8ddf29cbd7ca77b69a2d4827202908f27a
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-03-07 19:20:00 -0800

    Map logic

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,159 +1,146 @@
 import React, { useState, useEffect } from "react";
-
+import axios from "axios";
 import { Graph } from "react-d3-graph";
 
 const myConfig = {
-//   nodeHighlightBehavior: true,
-//   height: 240,
-//   width: 400,
-//   staticGraph: true,
-//   staticGraphWithDragAndDrop: false,
-  
-//   d3: {
-//     alphaTarget: 0,
-//     gravity: -400,
-//     linkLength: 500,
-//     linkStrength: 1,
-    
-//   },
-//   node: {
-//     renderLabel: false,
-//     color: "#35ff69",
-//     size: 100,
-//     highlightStrokeColor: "SAME",
-    
-    
-//   },
-//   link: {
-//     color: "#35ff69",
-//     highlightColor: "SAME",
-//     transformOrigin: 20
-//   }
-
-
-
-"automaticRearrangeAfterDropNode": true,
-  "collapsible": false,
-  "directed": true,
-  "focusAnimationDuration": 0.75,
-  "focusZoom": 1,
-  "height": 400,
-  "highlightDegree": 1,
-  "highlightOpacity": 1,
-  "linkHighlightBehavior": true,
-  "maxZoom": 8,
-  "minZoom": 0.1,
-  "nodeHighlightBehavior": false,
-  "panAndZoom": true,
-  "staticGraph": true,
-  "staticGraphWithDragAndDrop": false,
-  "width": 800,
-  "d3": {
-    "alphaTarget": 0.05,
-    "gravity": -100,
-    "linkLength": 100,
-    "linkStrength": 1,
-    "disableLinkForce": false
+  automaticRearrangeAfterDropNode: true,
+  collapsible: false,
+  directed: false,
+  focusAnimationDuration: 0.75,
+  focusZoom: 1,
+  highlightDegree: 1,
+  highlightOpacity: 1,
+  linkHighlightBehavior: true,
+  maxZoom: 5,
+  minZoom: 0.1,
+  nodeHighlightBehavior: true,
+  panAndZoom: false,
+  staticGraph: true,
+  staticGraphWithDragAndDrop: true,
+  d3: {
+    alphaTarget: 0,
+    gravity: -400,
+    linkLength: 180,
+    linkStrength: 1
   },
-  "node": {
-    "color": "#35ff69",
-    "fontColor": "yellow",
-    "fontSize": 8,
-    "fontWeight": "normal",
-    "highlightColor": "SAME",
-    "highlightFontSize": 8,
-    "highlightFontWeight": "normal",
-    "highlightStrokeColor": "SAME",
-    "highlightStrokeWidth": "SAME",
-    "labelProperty": "id",
-    "mouseCursor": "pointer",
-    "opacity": 4,
-    "renderLabel": false,
-    "size": 200,
-    "strokeColor": "blue",
-    "strokeWidth": 1.5,
-    "svg": "",
-    "symbolType": "none",
-    "transformOrigin" : 20 ,
-    "transform": 20
+  node: {
+    color: "#d3d3d3",
+    fontColor: "black",
+    fontSize: 12,
+    fontWeight: "normal",
+    highlightColor: "SAME",
+    highlightFontSize: 12,
+    highlightFontWeight: "bold",
+    highlightStrokeColor: "blue",
+    highlightStrokeWidth: "SAME",
+    mouseCursor: "pointer",
+    opacity: 1,
+    renderLabel: false,
+    size: 50,
+    strokeColor: "none",
+    strokeWidth: 2,
+    svg: "",
+    symbolType: "circle"
   },
-  "link": {
-    "color": "#d3d3d3",
-    "fontColor": "red",
-    "fontSize": 8,
-    "fontWeight": "normal",
-    "highlightColor": "SAME",
-    "highlightFontSize": 8,
-    "highlightFontWeight": "normal",
-    "labelProperty": "label",
-    "mouseCursor": "pointer",
-    "opacity": 1,
-    "renderLabel": true,
-    "semanticStrokeWidth": true,
-    "strokeWidth": 1.5,
-    "markerHeight": 6,
-    "markerWidth": 6
+  link: {
+    fontColor: "black",
+    fontSize: 12,
+    fontWeight: "normal",
+    highlightColor: "blue",
+    highlightFontSize: 8,
+    highlightFontWeight: "bold",
+    labelProperty: "label",
+    mouseCursor: "pointer",
+    opacity: 1,
+    renderLabel: true,
+    semanticStrokeWidth: true,
+    strokeWidth: 1.5
   }
-
-
-  
 };
 
-const onClickNode = function(nodeId) {
-    window.alert(`Clicked node ${nodeId}`);
-};
+const Map = ({ gameInfo }) => {
+  const validNodes = [];
+  const [mapRooms, setRooms] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-const onMouseOverNode = function(nodeId) {
-    window.alert(`Mouse over node ${nodeId}`);
-};
+  useEffect(() => {
+    axios.get("http://csp5.herokuapp.com/api/adv/rooms/").then(res => {
+      setRooms(res.data.rooms);
+      setLoading(false);
+    });
+  }, []);
 
-const onMouseOutNode = function(nodeId) {
-    window.alert(`Mouse out node ${nodeId}`);
-};
+  mapRooms.forEach(room => {
+    const directions = ["n_to", "s_to", "e_to", "w_to"];
+    directions.forEach(dir => {
+      if (room[dir]) {
+        validNodes.push(room[dir]);
+      }
+    });
+  });
 
-const onClickLink = function(source, target) {
-    window.alert(`Clicked link between ${source} and ${target}`);
-};
+  const southLinks = mapRooms
+    .filter(room => {
+      console.log(room);
+      if (validNodes.includes(room.id)) {
+        if (validNodes.includes(room.s_to)) {
+          return true;
+        }
+        return false;
+      }
+    })
+    .map(link => ({
+      source: link.id,
+      target: link.s_to
+    }));
 
-const onMouseOverLink = function(source, target) {
-    window.alert(`Mouse over in link between ${source} and ${target}`);
-};
+  const eastLinks = mapRooms
+    .filter(room => {
+      if (validNodes.includes(room.id)) {
+        if (validNodes.includes(room.e_to)) {
+          return true;
+        }
+        return false;
+      }
+    })
+    .map(link => ({
+      source: link.id,
+      target: link.e_to
+    }));
 
-const onMouseOutLink = function(source, target) {
-    window.alert(`Mouse out link between ${source} and ${target}`);
-};
+  const adjacentNodes = mapRooms.filter(room => validNodes.includes(room.id));
 
-
-
-const Map = ({ mapRooms, gameInfo }) => {
   const graph = {
     nodes: [
-        {id: `${gameInfo.name}`},
-        {id: `${gameInfo.title}`},
-      ],
-      links: [
-          {source: `${gameInfo.name}`, target: `${gameInfo.title}`},
-          
-      ]
-
-
-
+      ...mapRooms.map(room => {
+        return {
+          ...room,
+          x: room.x * 30,
+          y: room.y * -30,
+          color: room.id === gameInfo.room_id ? "#090909" : "#ffbd42",
+          strokeColor: room.id === gameInfo.room_id ? "#3F3F3F" : "none",
+          size: room.id === gameInfo.room_id ? 350 : "same"
+        };
+      }),
+      ...adjacentNodes.map(room => {
+        return {
+          ...room,
+          x: room.x * 30,
+          y: room.y * -30,
+          color: room.id === gameInfo.room_id ? "#090909" : "#ffbd42",
+          size: room.id === gameInfo.room_id ? 350 : 100,
+          strokeColor: room.id === gameInfo.room_id ? "#ffbd42" : "none"
+        };
+      })
+    ],
+    links: [...southLinks, ...eastLinks]
   };
+
+  if (loading) return <p>LOADING....</p>;
   return (
     <div className="node">
       {/* WIP */}
-    <Graph
-    id = {`${gameInfo.name}`}
-    data={graph} 
-    config={myConfig}
-    onClickNode={onClickNode}
-    onClickLink={onClickLink}
-    onMouseOverNode={onMouseOverNode}
-    onMouseOutNode={onMouseOutNode}
-    onMouseOverLink={onMouseOverLink}
-    onMouseOutLink={onMouseOutLink}
-         />
-         
+      <Graph id={`${gameInfo.name}`} data={graph} config={myConfig} />
     </div>
   );
 };
--- src/components/map/Room.js
+++ src/components/map/Room.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react'
-import './map.css';
+import React, { Component } from "react";
+import "./map.css";
 export class Room extends Component {
-    render() {
-        return (
-            <div className='room'>
-               {this.props.room.title} 
-               {this.props.room.description}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="room">
+        <h1>Current Room</h1>
+        {this.props.room.title}
+
+        <p>Description</p>
+        {this.props.room.description}
+      </div>
+    );
+  }
 }
 
-export default Room
+export default Room;
