4e1bd8f70df3719ac37d184d29ec1b8036822e62
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2020-03-09 17:48:26 -0700

    Update directions

--- src/components/gamePad/GamePad.js
+++ src/components/gamePad/GamePad.js
@@ -5,46 +5,43 @@ import axios from "axios";
 import triangle from "../../assets/icons/Triangle.svg";
 // http://csp5.herokuapp.com/api/adv/move/
 const GamePad = () => {
-
-  const onClickHandler =(e) => {
-
-  // e.preventDefault()
-  axios
-  .post("http://lambda-mud-test.herokuapp.com/api/adv/move/", {direction: e}, {
-    headers: {
-      authorization: `Token ${localStorage.getItem("token")}`
-    }
-  })
-  
-  }
-
-
-
-
+  const onClickHandler = e => {
+    // e.preventDefault()
+    axios.post(
+      "http://csp5.herokuapp.com/api/adv/move/",
+      { direction: e },
+      {
+        headers: {
+          authorization: `Token ${localStorage.getItem("token")}`
+        }
+      }
+    );
+  };
 
   return (
     <div className="gamepad-container">
-      <button className="control"
-      onClick= {e=> onClickHandler("n")}
-      >
+      <button className="control" onClick={e => onClickHandler("n")}>
         <img src={triangle} alt="move north control" />
       </button>
 
       <div className="gamepad-we-cardinal-container">
-        <button className="control control-left"
-         onClick= {e=> onClickHandler("e")}
+        <button
+          className="control control-left"
+          onClick={e => onClickHandler("w")}
         >
           <img src={triangle} alt="move west control" />
         </button>
-        <button className="control control-right"
-         onClick= {e=> onClickHandler("w")}
+        <button
+          className="control control-right"
+          onClick={e => onClickHandler("e")}
         >
           <img src={triangle} alt="move east control" />
         </button>
       </div>
 
-      <button className="control control-down"
-       onClick= {e=> onClickHandler("s")}
+      <button
+        className="control control-down"
+        onClick={e => onClickHandler("s")}
       >
         <img src={triangle} alt="move south control" />
       </button>
